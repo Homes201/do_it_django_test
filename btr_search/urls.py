90e84722111134d69f8b5a3d5e792b84050fafe2
sangjip <tkdwlqdl12@gmail.com>
2024-12-03 17:33:36 +0900

    Update landing page

--- btr_search/urls.py
+++ btr_search/urls.py
@@ -22,6 +22,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("single_pages.urls")),
     path("admin/", admin.site.urls),
     path("markdownx/", include("markdownx.urls")),
     path("accounts/", include("allauth.urls")),
